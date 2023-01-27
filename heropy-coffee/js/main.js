5afcef182b92cebe54dc50a14e9c87b10a4765c3
jinju <jinju000202@naver.com>
2023-01-27 09:50:51 +0900

    promotion slide modify / error 수정 전

--- heropy-coffee/js/main.js
+++ heropy-coffee/js/main.js
@@ -55,3 +55,19 @@ new Swiper('.notice .swiper', {
     autoplay: true, // 자동 재생 여부
     loop: true // 반복 재생 여부
   });
+
+  new Swiper('.promotion .swiper', {
+    autoplay: true,
+    loop: true,
+    slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이 여백
+    centeredslides: true, //1번 슬라이드가 가운데 보이기
+    pagination: { //페이지 번호 사용
+        el: '.promotion .swiper-pagination', //페이지 번호 요소
+        clickable: true //사용자의 페이지 번호 제어 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용
+        prevEl: '.promotion .swiper-button-prev',
+        nextEl: '.promotion .swiper-button-next'
+    }
+  });
