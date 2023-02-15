5404b5b9136cf88347ab5c70e10742f550e170b2
jinju <jinju000202@naver.com>
2023-02-15 19:34:44 +0900

    FOOTER

--- heropy-coffee/js/main.js
+++ heropy-coffee/js/main.js
@@ -127,3 +127,6 @@ new Swiper('.awards .swiper', {
     .setClassToggle(spyEl, 'show') //요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); //컨트롤러에 장면을 할당 (필수)
   });
+
+  const thisYear = document.querySelector('.this-year');
+  thisYear.textContent = new Date().getFullYear();
